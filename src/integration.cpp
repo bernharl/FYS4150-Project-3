eb2d44b93e9c24e209f0f895a15e7c7589009744
bernharl <bernharl@student.matnat.uio.no>
2019-10-03 13:16:14 +0200

    Restructured

--- src/integration.cpp
+++ src/integration.cpp
@@ -5,8 +5,8 @@ double int_func(double alpha, double x1, double y1, double z1, double x2, double
 {
     double r1 = sqrt(x1 * x1 + y1 * y1 + z1 * z1);
     double r2 = sqrt(x2 * x2 + y2 * y2 + z2 * z2);
-    double r12 = sqrt((x1 - x2) * (x1 * x2) + 
-                      (y1 - y2) * (y1 * y2) + 
+    double r12 = sqrt((x1 - x2) * (x1 * x2) +
+                      (y1 - y2) * (y1 * y2) +
                       (y1 - y2) * (y1 * y2));
     return exp(-2 * alpha * (r1 + r2)) / r12;
 
@@ -25,6 +25,7 @@ double gauleg_quad(double a, double b, int N, double alpha)
     for (int l = 0; l < N; l++){
     for (int m = 0; m < N; m++){
     for (int n = 0; n < N; n++){
+        cout << I << endl;
         I += w[i] * w[j] * w[k] * w[l] * w[m] * w[n]
              * int_func( alpha, x[i], x[j], x[k], x[l], x[m], x[n]);
     }}}}}}
@@ -34,15 +35,14 @@ double gauleg_quad(double a, double b, int N, double alpha)
 }
 
 int main()
-{   
+{
     double lambda = 10;
     double a = lambda;
     double b = - lambda;
     double alpha = 1;
     int N = 10;
-    
+
     double integral = gauleg_quad(a, b, N, alpha);
     cout << integral << endl;
     return 0;
 }
-
