932a8ef2465ae38b27ca732b3acfb75a33b3da3f
bernharl <bernharl@student.matnat.uio.no>
2019-10-05 01:11:04 +0200

    Changed position of sqrt, now works!

--- src/integration.cpp
+++ src/integration.cpp
@@ -1,6 +1,10 @@
 # include "weights.h"
 # define PI 3.1415926535897932384626433
 using std::cout;
+using std::cos;
+using std::sin;
+using std::sqrt;
+using std::exp;
 
 double int_func_cart(double alpha, double x1, double y1, double z1, double x2, double y2, double z2)
 {
@@ -13,7 +17,7 @@ double int_func_cart(double alpha, double x1, double y1, double z1, double x2, d
     //exit(0);
     //cout << exp(-2 * alpha * (r1 + r2)) / r12 << endl;
     //exit(0);
-    if (r12 <= EPS)
+    if (r12 <= ZERO)
     {
       //cout << "jeff" << endl;
       return 0;
@@ -23,25 +27,26 @@ double int_func_cart(double alpha, double x1, double y1, double z1, double x2, d
     return exp(-2 * alpha * (r1 + r2)) / r12;
 
 }
-double int_func_spherical(double alpha, double r1, double r2,
+double int_func_spherical(double u1, double u2,
                           double theta1, double theta2,
                           double phi1, double phi2)
 {
-
     double cos_b = cos(theta1) * cos(theta2) + sin(theta1) * sin(theta2) * cos(phi1 - phi2);
-    double r12 = sqrt(r1 * r1 + r2 * r2 - 2 * r1 * r2 * cos_b);
+    double r12 = u1 * u1 + u2 * u2 - 2 * u1 * u2 * cos_b;
+    //cout << r12 << endl;
     //cout << r12 << endl;
     //exit(0);
     //cout << exp(-2 * alpha * (r1 + r2)) / r12 << endl;
     //exit(0);
-    if (r12 <= EPS)
+    if (r12 <= ZERO)
     {
       //cout << "jeff" << endl;
+      //cout << r12 << endl;
       return 0;
     }
     //cout << "jeff" << endl;
     //cout << exp(-2 * alpha * (r1 + r2)) / r12 << endl;
-    return sin(theta1) * sin(theta2) / r12;
+    return 1.0 / sqrt(r12);
 
 }
 
@@ -70,44 +75,46 @@ double gauleg_quad(double a, double b, int N, double alpha)
 }
 
 double gauss_quad_improved(int N, double alpha)
-{
-    double *r = new double[N];
+{   double *u = new double[N+1];
     double *theta = new double[N];
     double *phi = new double[N];
 
-    double *w_r = new double[N];
+    double *w_u = new double[N+1];
     double *w_theta = new double[N];
     double *w_phi = new double[N];
 
 
     double I = 0;
-    gauss_laguerre(r, w_r, N, alpha);
+    gauss_laguerre(u, w_u, N+1, 2);
     gauleg(0, PI, theta, w_theta, N);
     gauleg(0, 2 * PI, phi, w_phi, N);
-    for (int i = 0; i < N; i++){
-    for (int j = 0; j < N; j++){
+    for (int i = 1; i <= N; i++){
+      cout << i <<": w_u= " << w_u[i] << ", u= " << u[i] << endl;
+      cout << i <<": w_theta= " << w_theta[i-1] << ", theta= " << theta[i-1] << endl;
+      cout << i <<": w_phi= " << w_phi[i-1] << ", phi= " << phi[i-1] << endl;
+      cout << i << " INTEGRAND XD JEFF! " << int_func_spherical(u[i], u[i], theta[i], theta[i], phi[i], phi[i]) << endl;
+    for (int j = 1; j <= N; j++){
     for (int k = 0; k < N; k++){
     for (int l = 0; l < N; l++){
     for (int m = 0; m < N; m++){
     for (int n = 0; n < N; n++){
-        //cout << I << endl;
-        //cout << int_func( alpha, x[i], x[j], x[k], x[l], x[m], x[n]) << endl;
-        I += w_r[i] * w_r[j] * w_theta[k] * w_theta[l] * w_phi[m] * w_phi[n]
-             * int_func_spherical(alpha, r[i], r[j], theta[k], theta[l], phi[m], phi[n]);
+        I += w_u[i] * w_u[j] * w_theta[k] * w_theta[l] * w_phi[m] * w_phi[n]
+             * int_func_spherical(u[i], u[j], theta[k], theta[l], phi[m], phi[n]) * sin(theta[k]) * sin(theta[l]);
+
+
+        //cout << int_func_spherical(alpha, u[i], u[j], theta[k], theta[l], phi[m], phi[n]) << endl;
     }}}}}}
 
 
-    delete [] r;
-    cout << "hei" << endl;
+    delete [] u;
     delete [] theta;
-    cout << "hei1" << endl;
     delete [] phi;
-    cout << "hei2" << endl;
 
-    delete [] w_r;
+    delete [] w_u;
     delete [] w_theta;
     delete [] w_phi;
-    return I;
+    cout << I << endl;
+    return I / (32 * pow(alpha, 5));
 }
 
 std::pair<double, double> monte_carlo(double a, double b, double N, double lambda, double alpha)
@@ -152,16 +159,17 @@ std::pair<double, double> monte_carlo(double a, double b, double N, double lambd
 
 int main()
 {
-    /*
-    double lambda = 1;
-    double a = - lambda;
-    double b = lambda;
+
+    //double lambda = 1;
+    //double a = - lambda;
+    //double b = lambda;
     double alpha = 2;
     int N = 20;
     //double integral = gauleg_quad(a, b, N, alpha);
     double integral = gauss_quad_improved(N, alpha);
-    cout << integral << " " << 5 * PI * PI / (16 * 16) << endl;
-    */
+    cout << integral << " " << 5 * PI * PI / (16 * 16) << " " << integral / (5 * PI * PI / (16 * 16)) <<  endl;
+
+    /*
     double lambda = 1.5;
     double a = - lambda;
     double b = lambda;
@@ -172,6 +180,7 @@ int main()
     double integral_MC = results_MC.first;
     double confidence_MC = results_MC.second;
     cout << integral_MC << " " << 5 * PI * PI / (16 * 16) << " " << confidence_MC << endl;
+    */
 
 
     return 0;
