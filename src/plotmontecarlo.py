a56f0fb559aa0e12af3cc193154ecb74d8a7f912
Nils-Ole Stutzer <nilsole2009@live.no>
2019-10-15 18:50:29 +0200

    Made plot code nicer in plotmontecarlo.py

--- src/plotmontecarlo.py
+++ src/plotmontecarlo.py
@@ -1,8 +1,21 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Setting fonts for pretty plot
+fonts = {
+    "font.family": "serif",
+    "axes.labelsize": 10,
+    "font.size": 10,
+    "legend.fontsize": 10,
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10,
+}
+plt.rcParams.update(fonts)
+
+# Analytical integral value
 analytical = 5 * np.pi**2 / 16**2
 
+# Loading data and defining errors
 data_brute = np.loadtxt("montecarlo.txt", skiprows = 1)
 data_improved = np.loadtxt("montecarlo_improved.txt", skiprows = 1)
 dataO1 = np.loadtxt("montecarlo_paro1.txt", skiprows = 1)
@@ -27,29 +40,34 @@ t_O1 = dataO1[:, 3]
 t_O2 = dataO2[:, 3]
 t_O3 = dataO3[:, 3]
 
-
+# Plotting errors
 fig, ax = plt.subplots()
-ax.loglog(N_brute, error_brute, label = "Brute")
+ax.loglog(N_brute, error_brute, label = "Brute force")
 ax.loglog(N_improved, error_improved, label = "Improved")
 ax.set_xlabel(r"$N$")
-ax.set_ylabel("relative error")
+ax.set_ylabel("Relative Error")
 ax.legend()
+fig.savefig("../doc/Figures/error_monte_carlo.eps", dpi=1000)
 
+# Plotting variance
 fig, ax = plt.subplots()
-ax.loglog(N_brute, var_brute, label = "Brute")
+ax.loglog(N_brute, var_brute, label = "Brute force")
 ax.loglog(N_improved, var_improved, label = "Improved")
 ax.set_xlabel(r"$N$")
 ax.set_ylabel("Variance")
 ax.legend()
+fig.savefig("../doc/Figures/variance_monte_carlo.eps", dpi=1000)
 
+# Plotting run time
 fig, ax = plt.subplots()
-ax.plot(N_brute, t_brute, label = "Brute")
+ax.plot(N_brute, t_brute, label = "Brute force")
 ax.plot(N_improved, t_improved, label = "Improved" )
 ax.set_xlabel(r"$N$")
 ax.set_ylabel("CPU time [ms]")
 ax.legend()
+fig.savefig("../doc/Figures/cpu_time_monte_carlo.eps", dpi=1000)
 
-
+# Plotting run time for three compiler flags
 fig, ax = plt.subplots()
 ax.plot(N_par, t_O1, label = "O1")
 ax.plot(N_par, t_O2, label = "O2" )
@@ -57,4 +75,5 @@ ax.plot(N_par, t_O3, label = "O3")
 ax.set_xlabel(r"$N$")
 ax.set_ylabel("CPU time [ms]")
 ax.legend()
+fig.savefig("../doc/Figures/cpu_time_compilerflag.eps", dpi=1000)
 plt.show()
