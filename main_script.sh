07ff548102aa40df0157afe2c4f2af1562e2f435
bernharl <bernharl@student.matnat.uio.no>
2019-10-18 08:28:20 +0200

    Added report building to main script

--- main_script.sh
+++ main_script.sh
@@ -15,7 +15,7 @@ read yn
 if [ "$yn" == "y" ] # If y, run c++ code
 then
 
-  ./mainprog.out $dim $tol $rhoN
+  ./mainprog.out 1
 fi
 
 echo "Generate plots? (y/n)"
@@ -24,4 +24,18 @@ if [ "$ynplot" == "y" ] # If y, run python script for plots
 then
   python3 plot1a.py
   python3 plotmontecarlo.py
+fi
+
+echo "Build report? (y/n)"
+read ynreport
+# If y, compile TeX document. The compilation is run many times because
+# bibtex is usually non-cooperative...
+if [ "$ynreport" == "y" ]
+then
+  cd ../doc/
+  pdflatex -synctex=1 -interaction=nonstopmode CompPhysProj3.tex
+  bibtex CompPhysProj3.aux
+  pdflatex -synctex=1 -interaction=nonstopmode CompPhysProj3.tex
+  bibtex CompPhysProj3.aux
+  pdflatex -synctex=1 -interaction=nonstopmode CompPhysProj3.tex
 fi
